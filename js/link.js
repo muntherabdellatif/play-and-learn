cd077c24fdb783a0c58050ec5742cf4ae23257ef
DESKTOP-8F9PJFG\AL-JWHARA <monthersaleh366@gmail.com>
2022-01-24 22:31:36 +0200

    adding img link

--- js/link.js
+++ js/link.js
@@ -24,25 +24,25 @@ const gamesLink = {
 const allGames = [
     {
         link :"https://gartic.io/" ,
-        img : "./img/Gartic.png" ,
+        img : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Fgartic.io%2Fphotos%2Fd41d8cd9%2F536042687000364%2F&psig=AOvVaw1vumSElSBmsE44xz-v-z5C&ust=1643142474163000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDz04Wdy_UCFQAAAAAdAAAAABAJ" ,
         name : "Gartic" , 
         description : "Draw and guess the words as fast as you can! Join on worldwide rooms or create your own." 
     } ,
     {
         link : "https://www.typingclub.com/sportal/program-3/116.play" ,
         img : "./img/typing-club.png" ,
-        name : "Typing Club" , 
+        name : "https://static.typingclub.com/m/corp2/lessonplan-icons/typing1.png" , 
         description : "Learn touch typing online using TypingClubs courses. It includes 650 typing games, typing tests and videos." 
     } ,
     {
         link : "https://flexboxfroggy.com/" ,
-        img : "./img/flexboxfroggy.png" ,
+        img : "https://retrogaming.altervista.org/wp-content/uploads/2018/10/maxresdefault.jpg" ,
         name : "Flexbox Froggy" , 
         description : " A game for learning CSS flexbox ." 
     } ,
     {
         link : "https://www.tinkercad.com/" ,
-        img : "./img/Tinkercad.jpg" ,
+        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzDTB7pol16eo0qlVC8nQ10bv2n7OtWbT0nQ&usqp=CAU" ,
         name : "Tinkercad" , 
         description : "Tinkercad is a free, easy-to-use web app that equips the next generation of designers and engineers with the foundational skills for innovation: 3D design, electronics, and coding!" 
     }
