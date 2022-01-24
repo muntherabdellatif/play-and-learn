1ac85fb509b67e4b6ec9e7b665a9b2bf308a93dc
DESKTOP-8F9PJFG\AL-JWHARA <monthersaleh366@gmail.com>
2022-01-24 22:27:18 +0200

    ok

--- js/link.js
+++ js/link.js
@@ -24,25 +24,25 @@ const gamesLink = {
 const allGames = [
     {
         link :"https://gartic.io/" ,
-        img : "../img/Gartic.png" ,
+        img : "./img/Gartic.png" ,
         name : "Gartic" , 
         description : "Draw and guess the words as fast as you can! Join on worldwide rooms or create your own." 
     } ,
     {
         link : "https://www.typingclub.com/sportal/program-3/116.play" ,
-        img : "../img/typing-club.png" ,
+        img : "./img/typing-club.png" ,
         name : "Typing Club" , 
         description : "Learn touch typing online using TypingClubs courses. It includes 650 typing games, typing tests and videos." 
     } ,
     {
         link : "https://flexboxfroggy.com/" ,
-        img : "../img/flexboxfroggy.png" ,
+        img : "./img/flexboxfroggy.png" ,
         name : "Flexbox Froggy" , 
         description : " A game for learning CSS flexbox ." 
     } ,
     {
         link : "https://www.tinkercad.com/" ,
-        img : "../img/Tinkercad.jpg" ,
+        img : "./img/Tinkercad.jpg" ,
         name : "Tinkercad" , 
         description : "Tinkercad is a free, easy-to-use web app that equips the next generation of designers and engineers with the foundational skills for innovation: 3D design, electronics, and coding!" 
     }
